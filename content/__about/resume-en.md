ba4d016bdf297575dc7f8109c0846a1f7012de82
Seunghyun Yu <hyun@mondrian.ai>
2020-04-04 16:58:22 +0900

    Edit blog

--- content/__about/resume-en.md
+++ content/__about/resume-en.md
@@ -4,10 +4,13 @@ date: 2019-1-27 16:21:13
 lang: 'en'
 ---
 
-# Your name
+# Seunghyun Yu
 
 <div align="center">
 
+yshyun123@mgial.com
+<div>
 _Thank you for reading my resume. If you want to contact me, Please send me an email._
+</div>
 
 </div>
--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  title: `seunghyun's devlog`,
+  description: `유승현의 개발 블로그입니다. 개인적으로 공부한 내용이나, 모아두었던 글을 공유하고 있습니다.`,
+  author: `Seunghyun Yu`,
+  introduction: `다른 사람들과 소통하고 함께 성장하고 싶은 프론트엔드 개발자입니다.`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
-    twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    twitter: `LaunchingDream`, // Your Twitter account
+    github: `LaunchingDream`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
